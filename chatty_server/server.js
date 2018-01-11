c8e9bc259b0b601e22bc56e312f1dc06f19a2df2
Bryan <bryanlampert@gmail.com>
2018-01-11 17:07:01 -0500

    Removed console log

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -77,7 +77,6 @@ wss.on('connection', (ws) => {
   // Client closes the connection
   // Updates the user counter
   ws.on('close', () => {
-    console.log('Client disconnected');
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify({ userCount: wss.clients.size }));
